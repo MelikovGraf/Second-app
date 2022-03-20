41f6283aa17973adff39865a1a2d3bbd66d3e5b7
Melikovgraf <Melikovgraf@yandex.ru>
2022-03-20 11:44:03 +0300

    Hello tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,43 @@
-const val MIN_COMMISSION = 35F
-const val COMMISSION = 0.0075F
 fun main() {
-    print("Input amount: ")
-    val amount = readLine()?.toFloat() ?: return
-    val resultCommission = amount * COMMISSION
-    val result = if (resultCommission <= MIN_COMMISSION)
-        MIN_COMMISSION
-    else resultCommission
-    val resultFinish: Int = result.toInt()
-    val resultCent = if (resultCommission <= MIN_COMMISSION)
-        0
-    else resultCommission - resultFinish
-    println("Коммиссия составит $resultFinish рублей $resultCent копеек")
+    while (true) {
+        print("\nВведите цифру типа карты: \n1. MasterCard  \n2. Мир \n3. VkPay\n")
+        val typeCard = readLine()?.toInt() ?: return
+        if (typeCard < 3 && typeCard > 0) {
+            print("Введите сумму предыдущий выплат: ")
+            val prevPay = readLine()?.toInt() ?: return
+            print("Введите сумму перевода: ")
+            val sumPay = readLine()?.toInt() ?: return
+            when (typeCard) {
+                1 -> println("Сумма к оплате с учетом коммиссии ${masterCard(prevPay, sumPay)}")
+                2 -> println("Сумма к оплате с учетом коммиссии ${mir(prevPay, sumPay)}")
+                3 -> println("Сумма к оплате с учетом коммиссии ${vk(prevPay, sumPay)}")
+            }
+        } else error("Error")
+    }
+}
+
+fun vk(
+    prevPay: Int = 0,
+    pay: Int
+): Int {
+    val result = if (pay < 15000 && prevPay < 40000) return pay
+    else error("Ошибка. Возможно превышен лимит")
+}
+
+fun masterCard(
+    prevPay: Int = 0,
+    pay: Int
+): Int {
+    val result = if (pay >= 300 && pay <= 7500) return pay
+    else if (pay < 150000 && prevPay < 600000 && pay > 0) return pay + (pay * 6 / 1000) + 20
+    else error("Ошибка. Возможно превышен лимит")
+}
+
+fun mir(
+    prevPay: Int = 0,
+    pay: Int
+): Int {
+    val result = if (pay >= 300 && pay <= 7500) return pay
+    else if (pay < 150000 && prevPay < 600000 && pay > 0) return pay + (pay * 75 / 10000)
+    else error("Ошибка. Возможно превышен лимит")
 }
